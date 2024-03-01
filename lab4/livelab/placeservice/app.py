ae53735c91fb3813f40d377118bbcea790f0a460
Indika Kumara <ipkumarawd@yahoo.com>
2024-03-01 12:24:13 +0100

    Fixed Place Record Service

--- lab4/livelab/placeservice/app.py
+++ lab4/livelab/placeservice/app.py
@@ -9,4 +9,4 @@ app = connexion.App(__name__, specification_dir="openapi/")
 app.add_api('place-service-api.yaml',
             arguments={'title': 'Place Service API'})
 Base.metadata.create_all(engine)
-app.run(host='0.0.0.0', port=5000, debug=True)
+app.run(host='0.0.0.0', port=5000)
