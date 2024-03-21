84231f72c6bc2bf30a24144b07c7a50202c6228c
Indika Kumara <ipkumarawd@yahoo.com>
2024-03-21 11:20:47 +0100

    Add Cache Control Header

--- lab6/orderrecordservice/app.py
+++ lab6/orderrecordservice/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
-
+from datetime import datetime, date, timedelta
+from functools import wraps
+from flask import Response
 from resources.order import Order, Orders
 
 app = Flask(__name__)
@@ -8,7 +10,26 @@ orders = Orders()
 placeRecord = Order()
 
 
+# Thanks https://www.maskaravivek.com/post/how-to-add-http-cachecontrol-headers-in-flask/
+def docache(minutes=5, content_type='application/json; charset=utf-8'):
+    """ Flask decorator that allow to set Cache headers. """
+
+    def fwrap(f):
+        @wraps(f)
+        def wrapped_f(*args, **kwargs):
+            r = f(*args, **kwargs)
+            then = datetime.now() + timedelta(minutes=minutes)
+            rsp = Response(r, content_type=content_type)
+            rsp.headers.add('Cache-Control', 'public,max-age=%d' % int(60 * minutes))
+            return rsp
+
+        return wrapped_f
+
+    return fwrap
+
+
 @app.route('/orders/<string:id>', methods=['GET'])
+@docache(minutes=1, content_type='application/json')
 def get_order(id):
     return placeRecord.get(id)
 
