257911a720f3cea6b1577246d1314f883442503b
Indika Kumara <ipkumarawd@yahoo.com>
2024-04-17 17:50:31 +0200

    Add Airflow Example for Live Lab

---
+++ lab10/airflow-example1/custom_ops.py
@@ -0,0 +1,17 @@
+from airflow.models import BaseOperator
+import pandas as pd
+import logging
+
+
+class CustomTransformationOperator(BaseOperator):
+    def __init__(self, file_name: str, **kwargs: object) -> None:
+        super().__init__(**kwargs)
+        self.file_name = file_name
+
+    def execute(self, context):
+        df = pd.read_csv(self.file_name)
+        logging.info(df)
+        df_return = df.groupby(['customer_id'])['unit_sales'].sum().reset_index()
+        file_returned = '/home/airflow/gcs/data/unit_sales_sum.csv'
+        df_return.to_csv(file_returned, index=False)
+        return file_returned
---
+++ lab10/airflow-example1/workflow.py
@@ -0,0 +1,67 @@
+import datetime
+
+from airflow import models
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.google.cloud.transfers.gcs_to_local import GCSToLocalFilesystemOperator
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+
+from custom_ops import CustomTransformationOperator
+
+# If you are running Airflow in more than one time zone
+# see https://airflow.apache.org/docs/apache-airflow/stable/timezone.html
+# for best practices
+YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
+
+default_args = {
+    'owner': 'ADA',
+    'depends_on_past': False,
+    'email': [''],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': datetime.timedelta(minutes=5),
+    'start_date': YESTERDAY,
+}
+
+with models.DAG(
+        'simple_etl',
+        catchup=False,
+        default_args=default_args,
+        schedule_interval=datetime.timedelta(days=1)) as dag:
+    download_from_gcs = GCSToLocalFilesystemOperator(
+        task_id="download_from_gcs",
+        object_name="sales.csv",
+        bucket="adadata",
+        filename="/home/airflow/gcs/data/sales.csv"
+    )
+
+    transform = CustomTransformationOperator(
+        task_id='transform_data', file_name='/home/airflow/gcs/data/sales.csv')
+
+    upload_to_gsc = LocalFilesystemToGCSOperator(
+        task_id='upload_to_gsc',
+        bucket='adadata',
+        src="{{ task_instance.xcom_pull(task_ids='transform_data') }}",
+        # See https://airflow.apache.org/docs/apache-airflow/stable/core-concepts/xcoms.html for Xcoms
+        dst='sales_sum.csv'
+    )
+
+    create_bq_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id='create_bq_dataset', dataset_id="sales"
+    )
+
+    load_to_table = GCSToBigQueryOperator(
+        task_id='load_to_bq_table',
+        bucket='adadata',
+        source_objects=['sales_sum.csv'],
+        destination_project_dataset_table="sales.summary",
+        schema_fields=[
+            {'name': 'customer_id', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+            {'name': 'unit_sales', 'type': 'INTEGER', 'mode': 'NULLABLE'}
+        ],
+        write_disposition='WRITE_TRUNCATE'
+    )
+
+    # About control flow, https://airflow.apache.org/docs/apache-airflow/stable/core-concepts/overview.html#control-flow
+    download_from_gcs >> transform >> upload_to_gsc >> create_bq_dataset >> load_to_table
