7675359143db0b315e579f890448601c0eb6cf99
Indika Kumara <ipkumarawd@yahoo.com>
2024-05-01 19:15:12 +0200

    Add Run from Local Example

---
+++ lab11/create_vm_from_local/main.tf
@@ -0,0 +1,65 @@
+provider "google" {
+  project     = var.project_id
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-custom-mode-network"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "my-custom-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+}
+# Create a single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = "ada-vm"
+  machine_type = "f1-micro"
+  zone         = "us-central1-c"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2204-jammy-v20240501"
+    }
+  }
+
+  # Install Flask
+  metadata_startup_script = "echo hi > /test.txt"
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.id
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
+
+resource "google_compute_firewall" "ssh" {
+  name = "allow-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  direction     = "INGRESS"
+  network       = google_compute_network.vpc_network.id
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
+}
+
+resource "google_compute_firewall" "flask" {
+  name    = "flask-app-firewall"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5000"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
---
+++ lab11/create_vm_from_local/variables.tf
@@ -0,0 +1,8 @@
+variable "project_id" {
+  type = string
+}
+
+variable "ada_github_uri" {
+  type = string
+  default = "https://github.com/IndikaKuma/ADA2024.git"
+}
