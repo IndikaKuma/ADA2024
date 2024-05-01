cf4b435f5c790d643232a46b4796a83cd75c0517
Indika Kumara <ipkumarawd@yahoo.com>
2024-05-01 18:26:17 +0200

    Update TF file

--- lab11/create_vm/create_vm.tf
+++ lab11/create_vm/create_vm.tf
@@ -12,7 +12,7 @@ resource "google_compute_subnetwork" "default" {
 }
 # Create a single Compute Engine instance
 resource "google_compute_instance" "default" {
-  name         = "flask-vm"
+  name         = "ada-vm"
   machine_type = "f1-micro"
   zone         = "us-west1-a"
   tags         = ["ssh"]
@@ -33,4 +33,28 @@ resource "google_compute_instance" "default" {
       # Include this section to give the VM an external IP address
     }
   }
+}
+
+resource "google_compute_firewall" "ssh" {
+  name = "allow-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  direction     = "INGRESS"
+  network       = google_compute_network.vpc_network.id
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
+}
+
+resource "google_compute_firewall" "flask" {
+  name    = "flask-app-firewall"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5000"]
+  }
+  source_ranges = ["0.0.0.0/0"]
 }
