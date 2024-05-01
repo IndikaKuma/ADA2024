0092d362c63f341d04e169b989ef33e14bb36cf0
Indika Kumara <ipkumarawd@yahoo.com>
2024-05-01 18:11:52 +0200

    Teraform Example

---
+++ lab11/create_vm/create_vm.tf
@@ -0,0 +1,36 @@
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-custom-mode-network"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "my-custom-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-west1"
+  network       = google_compute_network.vpc_network.id
+}
+# Create a single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = "flask-vm"
+  machine_type = "f1-micro"
+  zone         = "us-west1-a"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2204-jammy-v20240501"
+    }
+  }
+
+  # Install Flask
+  metadata_startup_script = "git clone https://github.com/IndikaKuma/ADA2024.git; cd ADA2024/installation_script"
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.id
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
